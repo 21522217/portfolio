62ad52bbba7559efcc6ceae31051a16c12118306
KIWI\ADMIN <21522217@gm.uit.edu.vn>
2025-08-16 10:09:30 +0700

    feat(dev): init primeng for home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  @HostBinding('class') className = '';
+
+  toggleTheme(theme: string) {
+    this.className = theme;
+  }
 }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [AvatarModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
